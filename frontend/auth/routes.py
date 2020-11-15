7ea93f07b098c88b01f97361fe4bc4a56f11010e
klima7 <ukasz.klimkiewicz2@wp.pl>
2020-11-15 22:08:42 +0100

    Dodano powiadomienia flash

--- frontend/auth/routes.py
+++ frontend/auth/routes.py
@@ -7,6 +7,7 @@ from .forms import LoginForm
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        print("Logging success")
+        flash('Login success')
         return redirect(url_for('main.index'))
+    flash('Log in now')
     return render_template('login.html', form=form)
