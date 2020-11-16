07cd6b333ec106e35659a2ab0943bb7d39381a2e
klima7 <ukasz.klimkiewicz2@wp.pl>
2020-11-16 15:48:11 +0100

    Kolejna wizualna poprawa powiadomień

--- frontend/auth/routes.py
+++ frontend/auth/routes.py
@@ -7,7 +7,7 @@ from .forms import LoginForm
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login success')
+        flash('Login success!')
         return redirect(url_for('main.index'))
-    flash('Log in now')
+    flash('Log in now!')
     return render_template('login.html', form=form)
