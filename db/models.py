0e954753762d1e30dfecd5ef9e2efddbfe4daef2
klima7 <ukasz.klimkiewicz2@wp.pl>
2020-11-16 22:13:54 +0100

    Przystosowanie bazy danych do rejestracji użytkowników

--- db/models.py
+++ db/models.py
@@ -1,6 +1,8 @@
 from . import _Base
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
@@ -9,11 +11,24 @@ class User(_Base, UserMixin):
 
     id = Column(Integer, primary_key=True)
     mail = Column(String, nullable=False, unique=True)
-    password_hash = Column(String, nullable=False)
+    password_hash = Column(String(128), nullable=False)
+    confirmed = Column(Boolean, default=False)
+    registration_data = Column(DateTime(), default=datetime.utcnow)
     packs = relationship("Pack", backref="user")
 
     def __repr__(self):
         return f"<User(id={self.id}, mail='{self.mail}', packs={self.packs})>"
+
+    @property
+    def password(self):
+        raise AttributeError('Password is read only')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Pack(_Base):
