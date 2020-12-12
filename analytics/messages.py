ba4210549b5df7c919dea95e66dd93f67d6f8f81
my name <my@email.com>
2020-12-12 22:26:25 +0100

    Wykres przybliżonej długości rozmowy

--- analytics/messages.py
+++ analytics/messages.py
@@ -183,9 +183,62 @@ def reply_time(data):
         reply_times = reply_times.append(t)
 
     reply_times = reply_times.dropna() # Usuwa osoby które nigdy nie odpowiedziały :(
+    reply_times = reply_times.sort_values(['time'], ascending=[0])
 
     gr = pygal.HorizontalBar()
     gr.add('', reply_times['time'].astype('int') / 1e9)
+    gr.x_labels = list(reply_times['user'])
+    gr.human_readable = True
+    gr.show_legend = False
+    gr.print_values = True
+    gr.print_values_position = 'top'
+
+    return gr
+
+
+def determine_conversation_length(sample, my_username): # sample to tabela wiadomości. W zamyśle są tam wiadomości tylko 2 użytkowników. (time, sender required)
+    # Słownik nazw użytkowników i ich unikalnych indeksów liczbowych.
+    senders = {k: v for v, k in enumerate(sample['sender'].unique())}
+
+    # Zamiana nazwy użytkownika na unikalnego, bo nie można użyć diff'a na stringach.
+    sample['sender2'] = sample['sender'].map(senders) 
+
+    # sample['sender2_test'] = sample['sender2'][::-1].diff() # Można usunąć (debug purposes)
+    sample['time2'] = sample['time'][::-1].diff() # Czas który upłynął od ostatniej wiadomości.
+
+    # Usuwanie kilku wiadomości użytkownika pod rząd. (Nie może przecież odpowiedzieć sam sobie)
+    # sample = sample[ (sample['sender2'][::-1].diff() != 0)[::-1]] 
+    # sample[:50]
+
+    # Odrzucanie wartości które są początkami rozmów. (Długi czas upłynął od ostatniej wiadomości. Improve if know how.)
+    sample['conversation_start'] = sample['time2'] > sample['time2'].mean()
+
+    conversation_begs = sample.index[sample['time2'] > sample['time2'].mean()].tolist()
+    sample.to_csv("dump.csv")
+
+    # indeks końca ostatniej rozmowy to -1
+    conversation_begs.insert(0, -1)
+    # indeks początku pierwszej rozmowy to len()-1
+    conversation_begs.insert(len(conversation_begs), len(sample) - 1)
+    conversation_begs
+    conversation_lens = [ conversation_begs[i] - conversation_begs[i - 1] for i in range(1, len(conversation_begs)) ]
+    return np.array(conversation_lens).mean()
+
+@graph('messages', _l('Average messages in conversation'))
+def conversation_length(data): # Skopiowane reply_time, więc nazwy zmiennych nie mają sensu
+    messages = data['table']
+    reply_times = pd.DataFrame();
+    convos = messages[messages['thread_type'] == 'Regular'].groupby('conversation')
+    for user, msg in convos:
+        t = pd.DataFrame({'user': [user], 'time': [determine_conversation_length(msg.loc[:, ('sender', 'time')], data['username'])]})
+    #     print(t)
+        reply_times = reply_times.append(t)
+
+    reply_times = reply_times.dropna() # Usuwa osoby które nigdy nie odpowiedziały :(
+    reply_times = reply_times.sort_values(['time'])
+
+    gr = pygal.HorizontalBar()
+    gr.add('', reply_times['time'])
     gr.x_labels = list(reply_times['user'])
     gr.human_readable = True
     gr.show_legend = False
