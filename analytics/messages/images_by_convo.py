433efdcb0d405ffabbb49dd12471f2dc16fca1b3
Jacek Stasiak <snsvgb@wp.pl>
2021-01-07 20:21:45 +0100

    Dodano wykres postów i zdjęć w konwersacji.

---
+++ analytics/messages/images_by_convo.py
@@ -0,0 +1,17 @@
+from .. import graph, style
+from flask_babel import gettext as _l
+import pygal
+import emoji
+
+MAX_PEOPLE_COUNT = 40
+
+@graph(_l('Number of photos in conversations'))
+def reacived_reactions(data):
+    msg = data['messages']
+    photo_count = msg.groupby(msg.conversation).photos.sum().sort_values().head(MAX_PEOPLE_COUNT)
+    photo_count = photo_count[photo_count != 0]
+    chart = pygal.HorizontalBar(style=style, show_legend=False)
+    chart.x_labels = list(photo_count.index)
+    chart.add('', photo_count)
+
+    return chart
---
+++ analytics/posts/posts_by_hour.py
@@ -0,0 +1,21 @@
+from .. import graph, style
+from flask_babel import gettext as _l
+import pygal
+import pandas as pd
+
+@graph(_l('percent of posts by hour'))
+def time_to_post(data):
+    post_data = data['posts']
+    total_posts_n = len(post_data)
+
+    posts_in_hour = post_data.groupby(post_data.time.dt.hour)
+    posts_in_hour = dict(posts_in_hour.count().time)
+    index = range(24)
+    values = [(posts_in_hour[i] / total_posts_n) * 100 if i in posts_in_hour else 0 for i in index]
+
+    radar_chart = pygal.Radar(style=style, show_legend=False, fill=True, height=800)
+    radar_chart.x_labels = index
+    radar_chart.add('', values)
+
+    return radar_chart
+
