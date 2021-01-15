97f81ea8c001fe22feb7e95d00379db9e62dbc79
Jacek Stasiak <snsvgb@wp.pl>
2021-01-15 23:17:42 +0100

    Zapomniałem dodać style w friends_pie

--- analytics/other/firends_pie.py
+++ analytics/other/firends_pie.py
@@ -15,7 +15,7 @@ def firends_pie(data):
     by_type = frens.groupby(frens.type)
     by_type = by_type.time.count()
 
-    pie_chart = pygal.Pie()
+    pie_chart = pygal.Pie(style=style)
     for k, v in zip(by_type.index, list(by_type)):
         pie_chart.add(k.replace("_", " "), v)
     pie_chart
