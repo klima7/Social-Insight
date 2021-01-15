a5d7bb709fe9c37f9b0bc4ed3820ee2c5da49713
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-01-15 18:05:10 +0100

    Dodano wykres typów wyszukiwanych elementów

---
+++ analytics/miscellaneous/search_type.py
@@ -0,0 +1,18 @@
+from .. import graph, style
+from flask_babel import gettext as _l
+import pygal
+import pandas as pd
+
+
+MAX_COUNT = 10
+
+
+@graph(_l('Types of most frequently searched elements'))
+def time_to_post(data):
+    history = data['search_history']
+    types = history.type.value_counts()
+    table = pd.DataFrame({'Search type': types.index, 'Count': types})
+    table = table.head(MAX_COUNT)
+
+    return table
+
