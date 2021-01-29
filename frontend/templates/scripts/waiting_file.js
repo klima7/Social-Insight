173f000fabda7049fdb9a541b0168baf66b581a8
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-01-29 19:47:07 +0100

    Reverse poprzedniego commitu Powoduje on pewne bugi, a nie jest niezbędny

--- frontend/templates/scripts/waiting_file.js
+++ frontend/templates/scripts/waiting_file.js
@@ -1,7 +1,4 @@
 let file_id = {{ file_id }};
-let visible_progress = 0;
-let real_progress = 0;
-let ready = false;
 
 function check() {
     $.ajax({
@@ -10,10 +7,13 @@ function check() {
         cache: false,
         success: function(data) {
 			if(data.progress != null) {
-				real_progress = data.progress;
-				ready = data.ready;
-				if(!ready)
-					setTimeout(check, 20);
+				$('#progress').text(data.progress)
+				if(data.ready) {
+					window.location.href = `/files/${file_id}/download`;
+					$('#message').text('{{ _('Here you are!') }} ')
+					setTimeout(() => window.close(), 2000);
+				}
+				else setTimeout(check, 20);
             }
         },
         error: function() {
@@ -21,19 +21,5 @@ function check() {
         }
     });
 }
-
-let intervalId = setInterval(function() {
-	if(visible_progress < real_progress) {
-		visible_progress += 1;
-		$('#progress').text(visible_progress)
-	}
-
-	if(visible_progress==100 && ready) {
-		window.location.href = `/files/${file_id}/download`;
-		$('#message').text('{{ _('Here you are!') }} ')
-		setTimeout(() => window.close(), 2000);
-		clearInterval(intervalId);
-	}
-}, 25);
 
 check();
