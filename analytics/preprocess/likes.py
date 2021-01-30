a6847d1b79464b5417494a1254743eaa9e677792
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-01-30 01:26:29 +0100

    Usunięte niepotrzebne łapanie wyjątku

--- analytics/preprocess/likes.py
+++ analytics/preprocess/likes.py
@@ -9,23 +9,19 @@ def _get_likes_data(zip_file, folders):
 
     paths = ['likes_and_reactions/posts_and_comments.json', 'likes_and_reactions/pages.json']
     for path in paths:
-        try:
-            # Likes on posts
-            with zip_file.open('likes_and_reactions/posts_and_comments.json') as f:
-                jdata = json.loads(f.read())
+        # Likes on posts
+        with zip_file.open('likes_and_reactions/posts_and_comments.json') as f:
+            jdata = json.loads(f.read())
 
-                likes = []
-                times = []
+            likes = []
+            times = []
 
-                for i in jdata['reactions']:
-                    times.append(i['timestamp'])
-                    likes.append(i['data'][0]['reaction']['reaction'])
+            for i in jdata['reactions']:
+                times.append(i['timestamp'])
+                likes.append(i['data'][0]['reaction']['reaction'])
 
-                temp_table = pd.DataFrame({'time': times, 'type': likes})
-                temp_table.time = pd.to_datetime(temp_table.time, unit='s')
-                like_table = pd.concat([like_table, temp_table])
-        except KeyError as e:
-            pass
-            # print("No likes or reactions")
+            temp_table = pd.DataFrame({'time': times, 'type': likes})
+            temp_table.time = pd.to_datetime(temp_table.time, unit='s')
+            like_table = pd.concat([like_table, temp_table])
 
     return like_table
